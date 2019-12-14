9e524aab03eb1cac877279ede90a987335b6a8a4
Ian Schoonover <iantskon@gmail.com>
2019-12-14 11:58:14 -0600

    Add https redirect

--- app.js
+++ app.js
@@ -54,6 +54,17 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// force https
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
+app.use(requireHTTPS);
+
 // async function createOneUser() {
 // 	let admin = await User.findOne({'username': 'admin'});
 // 	if (!admin) {
--- routes/index.js
+++ routes/index.js
@@ -11,17 +11,17 @@ const puppeteer = require('puppeteer-extra');
 const pluginStealth = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(pluginStealth());
 
-router.get('/', function(req, res, next) {
-  res.redirect('/courses');
-});
-
-router.get('/courses', async function(req, res, next) {
+router.get('/', async function(req, res, next) {
 	try {
 		const courses = await Course.find();
 	  res.render('index', { title: 'Top Rated Technology Courses on Udemy', courses });
 	} catch(err) {
 		res.status(500).send('Oops, something broke!');
 	}
+});
+
+router.get('/courses', function(req, res, next) {
+  res.redirect('/');
 });
 
 router.get('/courses/new', isAdminLoggedIn, function(req, res, next) {
@@ -57,7 +57,7 @@ router.post('/courses', isAdminLoggedIn, async function(req, res, next) {
 			courseInfo.affiliateUrl = req.body.affiliateUrl;
 		  await Course.create(courseInfo);
 		  req.session.success = 'Course created successfully!';
-		  res.redirect('/courses');
+		  res.redirect('/');
 	} catch(err) {
 	  req.session.error = err.message;
 	  res.redirect('/courses/new');
@@ -83,7 +83,7 @@ router.put('/courses/:id', isAdminLoggedIn, function(req, res, next) {
 	Course.findByIdAndUpdate(req.params.id, req.body, {new:true})
 		.then(course => {
 			console.log('Course:', course);
-		  res.redirect('/courses');
+		  res.redirect('/');
 		})
 		.catch(err => {
 			throw new Error(err);
@@ -98,46 +98,46 @@ router.get('/login', function(req, res, next) {
 
 router.get('/logout', function(req, res) {
   req.logout();
-  res.redirect('/courses');
+  res.redirect('/');
 });
 
-router.get('/courses/update', isAdminLoggedIn, async function(req, res, next) {
-	const courses = await Course.find();
-  try {
-		for (const course of courses) {	
-			// puppeteer usage as normal
-			const browser = await puppeteer.launch({ 
-				headless: true,
-				'args' : [
-		    '--no-sandbox',
-		    '--disable-setuid-sandbox'
-		  ]});
-		  const page = await browser.newPage()
-		  await page.setViewport({ width: 800, height: 600 })
-		  await page.goto(course.affiliateUrl)
-		  await page.waitForSelector('.course-price-text > span + span > span');
-		  const title = await page.$eval('h1[data-purpose=lead-title]', e => e.innerText);
-		  // await page.screenshot({ path: title, fullPage: true })
-		  const thumbnailUrl = await page.$eval('div[data-purpose=introduction-asset] img', e => e.src);
-		  const listPrice = await page.$eval('.course-price-text > span + span > span', e => e.innerText);
-		  const ogPrice = await page.$eval('div[data-purpose=course-old-price-text] > span + span > s > span', e => e.innerText);
-		  const percentOff = await page.$eval('div[data-purpose=discount-percentage] > span + span', e => e.innerText);
-		  const rating = await page.$eval('div[data-purpose=ratings] div.rate-count > span > span', e => e.innerText);
-		  await browser.close()
-		  courseInfo = {title, listPrice, percentOff, ogPrice, thumbnailUrl, rating};
-		  await Course.findByIdAndUpdate(course._id, courseInfo);
-		}
-  } catch(err) {
-    req.session.error = err.message;
-    res.redirect('/courses');
-  }
-  req.session.success = 'Courses updated successfully!';
-  res.redirect('/courses');
-});
+// router.get('/courses/update', isAdminLoggedIn, async function(req, res, next) {
+// 	const courses = await Course.find();
+//   try {
+// 		for (const course of courses) {	
+// 			// puppeteer usage as normal
+// 			const browser = await puppeteer.launch({ 
+// 				headless: true,
+// 				'args' : [
+// 		    '--no-sandbox',
+// 		    '--disable-setuid-sandbox'
+// 		  ]});
+// 		  const page = await browser.newPage()
+// 		  await page.setViewport({ width: 800, height: 600 })
+// 		  await page.goto(course.affiliateUrl)
+// 		  await page.waitForSelector('.course-price-text > span + span > span');
+// 		  const title = await page.$eval('h1[data-purpose=lead-title]', e => e.innerText);
+// 		  // await page.screenshot({ path: title, fullPage: true })
+// 		  const thumbnailUrl = await page.$eval('div[data-purpose=introduction-asset] img', e => e.src);
+// 		  const listPrice = await page.$eval('.course-price-text > span + span > span', e => e.innerText);
+// 		  const ogPrice = await page.$eval('div[data-purpose=course-old-price-text] > span + span > s > span', e => e.innerText);
+// 		  const percentOff = await page.$eval('div[data-purpose=discount-percentage] > span + span', e => e.innerText);
+// 		  const rating = await page.$eval('div[data-purpose=ratings] div.rate-count > span > span', e => e.innerText);
+// 		  await browser.close()
+// 		  courseInfo = {title, listPrice, percentOff, ogPrice, thumbnailUrl, rating};
+// 		  await Course.findByIdAndUpdate(course._id, courseInfo);
+// 		}
+//   } catch(err) {
+//     req.session.error = err.message;
+//     res.redirect('/courses');
+//   }
+//   req.session.success = 'Courses updated successfully!';
+//   res.redirect('/courses');
+// });
 
 router.post('/login', passport.authenticate('local'), function(req, res, next) {
 	req.session.success = 'Welcome! ' + req.user.username;
-  res.redirect('/courses');
+  res.redirect('/');
 });
 
 function isAdminLoggedIn(req, res, next) {
