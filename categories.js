df6cc03969ac4908e003e9ac1577dd057f18cbdf
Ian Schoonover <iantskon@gmail.com>
2020-01-24 20:09:28 -0600

    Update categories and sort courses

--- categories.js
+++ categories.js
@@ -1,9 +1,14 @@
 module.exports = [
 	{name: 'web', formal: 'Web Development'},
 	{name: 'cs', formal: 'Computer Science'},
+	{name: 'algosds', formal: 'Algos & DS'},
 	{name: 'mobile', formal: 'Mobile Development'},
 	{name: 'frontend', formal: 'Front-end Frameworks'},
 	{name: 'gaming', formal: 'Gaming Development'},
 	{name: 'design', formal: 'Design'},
-	{name: 'dbs', formal: 'Databases'}
+	{name: 'dbs', formal: 'Databases'},
+	{name: 'datascience', formal: 'Data Science'},
+	{name: 'ml', formal: 'Machine Learning'},
+	{name: 'devops', formal: 'DevOps'},
+	{name: 'testing', formal: 'Testing'},
 ];
